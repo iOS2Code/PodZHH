225987110155355896d410e75b7f961ff868c403
iOS-朱鸿 <zhuhong@bbdtek.com>
2017-02-16 14:56:05 +0800

    add Other Pod

---
+++ OtherPod/Public.m
@@ -0,0 +1,17 @@
+//
+//  Public.m
+//  Public
+//
+//  Created by  ZhuHong on 2017/2/16.
+//  Copyright © 2017年 BBDTEK. All rights reserved.
+//
+
+#import "Public.h"
+
+@implementation Public
+
++ (void)publicTest {
+    NSLog(@"CoderHG 最帅了 : %@", NSStringFromSelector(_cmd));
+}
+
+@end
